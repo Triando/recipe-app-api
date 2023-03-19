8f080c7769592687bf13bcb8624b0e32be0ee609
Triando <Triando@Gmail.com>
2023-03-18 17:10:21 -0700

    Finish the tags API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,9 +7,11 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+
 def create_user(email='user@example.com', password='testpass123'):
     """Helper function to create a new user"""
     return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     """Tests for models"""
