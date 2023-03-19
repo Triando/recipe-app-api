bfe2c4058f213add9954f59be61a66a83f65fbe6
Triando <Triando@Gmail.com>
2023-03-19 15:04:45 -0700

    fixed

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,7 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = [
-            'id', 'title', 'time_minutes', 'price', 'link', 'tags'
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags',
             'ingredients'
         ]
         read_only_fields = ['id', ]
